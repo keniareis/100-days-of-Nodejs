8c31c03344398f36bb05350d0c6958e198a33332
keniareis <keniapubg@gmail.com>
2024-11-28 10:42:35 -0300

    Day06: connect db to application

--- Day06/server.js
+++ Day06/server.js
@@ -1,7 +1,16 @@
 import app from './src/app.js'
+import conexao from './infra/conexao.js'
 
 const PORT = 3000;
 
-app.listen(PORT, () =>{
-    console.log(`ouvindo a porta ${PORT}`);
-});
+//fazer a conexao
+conexao.connect((erro) =>{
+    if(erro){
+        console.log(erro);
+    }else{
+        console.log('Sucesso na conexão!');
+        app.listen(PORT, () =>{
+            console.log(`ouvindo a porta ${PORT}`);
+        });
+    }
+})
